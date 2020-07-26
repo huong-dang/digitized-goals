669330e92ac0988e4ddfd0c81e354f6a849c901d
Huong Dang <huongd97@knights.ucf.edu>
2020-07-25 22:00:37 -0400

    Add material ui into project, add foundation for sign in page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Grid, Card, TextField, Button } from "@material-ui/core";
 
-export default () => <div>Hello world!</div>;
+function Home(props) {
+    return (
+        <Fragment>
+            <form>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <TextField label="Username" />
+                    <TextField label="Password" type="password" />
+                    <Button>Sign In</Button>
+                </Grid>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Home;
