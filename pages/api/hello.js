2e2bf0b5ea1cd695e01a086ae90d39b72c6c221b
Huong Dang <huongd97@knights.ucf.edu>
2020-07-27 15:44:14 -0400

    trying call database instead

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,9 @@
+const db = require("../../data/database");
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-    console.log("hello world!!!");
+    db.initialize();
     res.statusCode = 200;
-    res.json({ name: "John Doe" });
+    res.json({ name: "Yay! Successfully connected to databse" });
 };
